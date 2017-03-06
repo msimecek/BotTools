12279b0f6cd5e7127e29b7a2a0d6e70cdad51184
Martin Simecek <msimecek@users.noreply.github.com>
2017-03-06 15:43:23 +0100

    BotMessages

---
+++ Helpers/BotMessages.cs
@@ -0,0 +1,30 @@
+using Microsoft.Bot.Builder.ConnectorEx;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BotTools.Helpers
+{
+    public static class BotMessages
+    {
+        public static async Task PostTyping(this IDialogContext context)
+        {
+            var typingMessage = context.MakeMessage();
+            typingMessage.Type = ActivityTypes.Typing;
+            typingMessage.Text = null;
+            await context.PostAsync(typingMessage);
+        }
+
+        public static async Task PostWithQuickRepliesAsync(this IDialogContext context, string text, params FacebookQuickReply [] replies)
+        {
+            var message = context.MakeMessage();
+            message.Text = text;
+            message.ChannelData = new FacebookMessage(text, replies);
+            await context.PostAsync(message);
+        }
+    }
+}
